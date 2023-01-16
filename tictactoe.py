c258c4aefd1844bf8de4e94fbcf332049dd55556
ForskyOnly <foulseumsky@gmail.com>
2023-01-16 09:29:31 +0100

    brnch

--- tictactoe.py
+++ tictactoe.py
@@ -1,28 +1,29 @@
-# dico = {
-#     "A" : ["_","_","_"],
-#     "B" : ["_","_","_"],
-#     "C" : ["_","_","_"]
-# }
+dico = {
+    "A" : ["_","_","_"],
+    "B" : ["_","_","_"],
+    "C" : ["_","_","_"]
+}
 
 
-# def afficher_la_grille():
-#     """
-#     fonction qui affiche une grille crée avec un dictionnaire auparavant
-#     et qui fais la mise en forme des elements, séparés d'un "|", avec un retour
-#     à la ligne, aprés chaque element de la clé du dictionnaire.    
-#     """
-#     for cle in dico:
-#         for elt in dico [cle]:
-#             print(" ", elt, " |", end = "")
-#         print("\n")
+def afficher_la_grille():
+    """
+    fonction qui affiche une grille crée avec un dictionnaire auparavant
+    et qui fais la mise en forme des elements, séparés d'un "|", avec un retour
+    à la ligne, aprés chaque element de la clé du dictionnaire.    
+    """
+    for cle in dico:
+        for elt in dico [cle]:
+            print(" ", elt, " |", end = "")
+        print("\n")
     
-# afficher_la_grille()
+afficher_la_grille()
 
-# def jouer_un_coup():
-#     val_user = (input("choisissez votre position : "))
-#     if val_user[0] in ["A","B","C"] and (val_user[1]) in [1,2,3] :
-#         if dico[val_user[0]][(val_user[1])] == "_" :
-#             return(dico[val_user[0]][(val_user[1])] == "x")
+def jouer_un_coup():
+    val_user = (input("choisissez votre position : "))
+    if val_user[0] in ["A","B","C"] and (val_user[1]) in [1,2,3] :
+        if dico[val_user[0]][(val_user[1])] == "_" :
+            return(dico[val_user[0]][(val_user[1])] == "x")
+        
             
          
         
@@ -42,30 +43,6 @@
     
        
 # jouer_un_coup()
-
-board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-         'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-         'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
-
-def print_board(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
-    print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
-    print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
-
-turn = 'X'
-for i in range(9):
-    print_board(board)
-    print(f"Turn for {turn}. Move on which space?")
-    move = input()
-    board[move] = turn
-    if turn == 'X':
-        turn = 'O'
-    else:
-        turn = 'X'
-
-print_board(board)
 
 
    
